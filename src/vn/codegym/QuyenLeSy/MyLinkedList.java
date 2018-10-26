81650c8537858f791fb23291a9d904a858a1e099
Quyen Le Sy <danquyen6596@gmail.com>
2018-10-26 19:27:13 +0700

    Add file to program.

---
+++ src/vn/codegym/QuyenLeSy/MyLinkedList.java
@@ -0,0 +1,84 @@
+package vn.codegym.QuyenLeSy;
+
+public class MyLinkedList<E> {
+    private Node head;
+    private int numNodes;
+
+    public class Node{
+        private Node next;
+        private Object data;
+
+        public MyLinkList(Object data) {
+            head = new Node(data);
+        }
+
+        public Node(Object data){
+            this.data = data;
+        }
+
+        public Object getData() {
+            return data;
+        }
+
+        public void add(int index, E element){
+            
+        }
+
+        public void addFirst(E e){
+
+        }
+
+        public void addLast(E e){
+
+        }
+
+        public E remove(int index){
+
+        }
+
+        public boolean remove(Object e){
+
+        }
+
+        public int size(){
+
+        }
+
+        public E clone(){
+
+        }
+
+        public boolean contains(E o){
+
+        }
+
+        public indexOf(E o){
+
+        }
+
+        public boolean add(E o){
+
+        }
+
+        public void ensureCapacity(int minCapacity){
+
+        }
+
+        public E get(int i){
+
+        }
+
+        public E getFirst(){
+
+        }
+
+        public E getLast(){
+
+        }
+
+        public void clear(){
+
+        }
+
+    }
+}
---
+++ src/vn/codegym/QuyenLeSy/MyLinkedListTest.java
@@ -0,0 +1,7 @@
+package vn.codegym.QuyenLeSy;
+
+public class MyLinkedListTest {
+    public static void main(String[] args) {
+
+    }
+}
